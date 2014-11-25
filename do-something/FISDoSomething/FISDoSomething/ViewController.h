3892c95961df8f9c5cca01408bc13eb92c86eadb
dbarabander <barabander@gmail.com>
2014-11-25 09:41:27 -0500

    created empty project

---
+++ do-something/FISDoSomething/FISDoSomething/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FISDoSomething
+//
+//  Created by DANIEL BARABANDER on 11/25/14.
+//  Copyright (c) 2014 Flatiron iOS 003. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ do-something/FISDoSomething/FISDoSomething/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FISDoSomething
+//
+//  Created by DANIEL BARABANDER on 11/25/14.
+//  Copyright (c) 2014 Flatiron iOS 003. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
